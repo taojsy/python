0babc9378b188bce3c118891ffa023ef649125a6
taojsy <58379046+taojsy@users.noreply.github.com>
2020-01-01 19:26:52 +0800

    Create setup.py

---
+++ mymodules/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='vsearch',
+    version='1.0',
+    description='The Search Tools',
+    author='Tao Jun',
+    author_email='taojsy@sina.com',
+    url='headfirstlabs.com',
+    py_modules=['vsearch'],
+    )
